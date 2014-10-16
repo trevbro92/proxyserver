d6e6b328d2484b521189176b0163a04795e325ae
Trevor Broaddus <tab5b@mtmail.mtsu.edu>
2014-10-16 15:51:58 -0500

    first commit

---
+++ ProxyServer/ProxyServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ProxyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest req = (WebRequest.Create("http://www.cs.mtsu.edu")) as HttpWebRequest;
+           
+
+
+        }
+    }
+}
